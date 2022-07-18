37271638cfc4a3261d4a2ba22e1489beb6496b93
YenumulaNaveen <Yenumula.Naveen@ibm.com>
2022-07-18 12:00:30 +0530

    file-io

---
+++ file-io/src/FileReaderDemo.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+	public static void main(String[] args) {
+		String path = "C:\\Users\\003S5R744\\Downloads\\Git.txt";
+		BufferedReader reader = null;
+		String line = null;
+		
+		try {
+			FileReader fr = new FileReader(path);	// opening file stream
+			reader = new BufferedReader(fr);	// Wrapping buffer around it
+			
+			while(true) {
+				line = reader.readLine();	// Reads a line from buffer
+				if(line == null)		// EOF
+					break;
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(reader != null)
+					reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
