3dd482dc690e5df5b9cde3ab39038bdc0349fd1b
YenumulaNaveen <Yenumula.Naveen@ibm.com>
2022-07-18 11:58:41 +0530

    collection

---
+++ collection-api/src/StresmDemo3.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StresmDemo3 {
+	
+	private static void process(int i) {
+		try {
+			Thread.sleep(1); // lets thread sleep for a millisecond
+		} catch (InterruptedException e) {}
+	}
+	
+	public static void main(String[] args) {
+
+		List<Integer> numbers = IntStream.range(1, 99).boxed().collect(Collectors.toList());
+		
+		long startTime = System.currentTimeMillis();
+		numbers.stream().forEach(StresmDemo3::process);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Time taken by sequential stream: " + (endTime - startTime));
+		
+		startTime = System.currentTimeMillis();
+		numbers.parallelStream().forEach(StresmDemo3::process);
+		endTime = System.currentTimeMillis();
+		System.out.println("Time taken by parallel stream: " + (endTime - startTime));
+	}
+
+}
